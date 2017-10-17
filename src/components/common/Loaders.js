db77222160b081914d22a00dab4a339ca4d18ed3
Cijo KB <cijo.kb@gmail.com>
2017-10-17 08:33:00 +0530

    Update Loaders.js
    removing commented code

--- src/components/common/Loaders.js
+++ src/components/common/Loaders.js
@@ -1,24 +1,3 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-//
-// const Loaders = ({show}) => (
-//     <div className="loader" hidden={!show}>
-//         <svg viewBox="0 0 32 32" width="32" height="32">
-//             <circle id="spinner" cx="16" cy="16" r="14" fill="none"/>
-//         </svg>
-//     </div>
-// );
-//
-// // Specifies the default values for props:
-// Loaders.defaultProps = {
-//     show: false
-// };
-//
-// Loaders.propTypes = {
-//     show: PropTypes.bool
-// };
-//
-// export default Loaders;
 
 import React from 'react';
 import RefreshIndicator from 'material-ui/RefreshIndicator';
