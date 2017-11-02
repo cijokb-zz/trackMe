dfde0c0ff5a0d5073d622c32d26a58d72d4f1b84
cijo.kb <cijo.kb@accenture.com>
2017-11-02 09:38:44 +0530

    created a dummy footer component issue#10

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import FontIcon from 'material-ui/FontIcon';
 import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
 import Paper from 'material-ui/Paper';
@@ -7,28 +7,30 @@ import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
 const recentsIcon = <FontIcon className="material-icons">restore</FontIcon>;
 const favoritesIcon = <FontIcon className="material-icons">favorite</FontIcon>;
 const nearbyIcon = <IconLocationOn />;
-const style = {bottom: 0, position: 'fixed'};
+const style = {bottom: 0, position: 'fixed', display: 'block', width: '100%'};
 
 const Footer = () => (
-    <Paper zDepth={1} style={style}>
-        <BottomNavigation selectedIndex={0}>
-            <BottomNavigationItem
-                label="Recents"
-                icon={recentsIcon}
-                onClick={() => window.alert('todo')}
-            />
-            <BottomNavigationItem
-                label="Favorites"
-                icon={favoritesIcon}
-                onClick={() => window.alert('todo')}
-            />
-            <BottomNavigationItem
-                label="Nearby"
-                icon={nearbyIcon}
-                onClick={() => window.alert('todo')}
-            />
-        </BottomNavigation>
-    </Paper>
+    <div style={style}>
+        <Paper zDepth={1}>
+            <BottomNavigation selectedIndex={0}>
+                <BottomNavigationItem
+                    label="Recents"
+                    icon={recentsIcon}
+                    onClick={() => console.log('todo')}
+                />
+                <BottomNavigationItem
+                    label="Favorites"
+                    icon={favoritesIcon}
+                    onClick={() => console.log('todo')}
+                />
+                <BottomNavigationItem
+                    label="Nearby"
+                    icon={nearbyIcon}
+                    onClick={() => console.log('todo')}
+                />
+            </BottomNavigation>
+        </Paper>
+    </div>
 );
 
 export default Footer;
