c3114c16d9103c0fbdbba7b40bebab1f005cf57f
cijo.kb <cijo.kb@accenture.com>
2018-04-10 19:16:05 +0530

    Removed Date.now from reducers and also temporary design doc issue#89

--- src/reducers/snackBarReducer.js
+++ src/reducers/snackBarReducer.js
@@ -4,7 +4,7 @@ import initialState from './initialState';
 export default function snackBarReducer (state = initialState.snackBarMsg, action) {
     switch (action.type) {
     case types.SHOWING_SNACKBAR:
-        return Object.assign({}, state, {message: action.message, timeStamp: Date.now()});
+        return Object.assign({}, state, {message: action.message});
     default:
         return state;
     }
